27cf6178b704c760a1b7e833ef76a4e19aa9e22f
Jose Antonio <gokugamer243@gmail.com>
2023-02-03 13:04:42 +0100

    Add: Pagina principal

---
+++ index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+import path from "path";
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+import apiRoutes from "./routes";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const DB_URI = process.env.DB_URI;
+
+mongoose.connect(DB_URI)
+    .then( db => console.log("Conexion a la base de datos de armeria correcta"))
+    .catch(error => console.log("Error: " + error));
+
+app.use(cors());
+app.use(express.json());
+app.use("/api", apiRoutes);
+
+app.listen(PORT, () => console.log("Servidor iniciado."));
