0667a131e85f786facd4bfa89498712aaf46092b
Jose Antonio <gokugamer243@gmail.com>
2023-02-06 16:47:30 +0100

    Add: Actualizar cliente

--- routes.js
+++ routes.js
@@ -52,4 +52,18 @@ router.get("/armeria/clientes/:id", cors(), (req, res) => {
     );
 })
 
+router.put("/armeria/clientes/:id", cors(), (req, res) => {
+    Cliente.findByIdAndUpdate(
+        { _id: req.params.id },
+        { $set: { nombre: req.body.nombre, apellido1: req.body.apellido1, apellido2: req.body.apellido2 } },
+        (err, data) => {
+            if (err) {
+                res.json({ error: err });
+            } else {
+                res.json(data);
+            }
+        }
+    );
+})
+
 export default router;
