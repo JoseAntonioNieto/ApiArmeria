1bcfbcbaff7c9fbd95f8d1069c6eceb6e87cc0d1
Jose Antonio <gokugamer243@gmail.com>
2023-02-09 17:24:33 +0100

    Add: Get de clientes documentado

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
 import dotenv from "dotenv";
-import path from "path";
 import cors from "cors";
 import express from "express";
 import mongoose from "mongoose";
 import apiRoutes from "./routes.js";
+import bodyParser from "body-parser";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const DB_URI = process.env.DB_URI;
+
+// Swagger
+const options = {
+    definition: {
+      info: {
+        title: "Documentacion de Api Armeria",
+        version: "0.1.0",
+        description:
+          "Esta es la documentacion de la api de la Armeria",
+        license: {
+          name: "MIT",
+          url: "https://spdx.org/licenses/MIT.html",
+        },
+        contact: {
+          name: "Jose Antonio",
+          url: "https://github.com/JoseAntonioNieto",
+          email: "gokugamer243@gmail.com",
+        },
+      },
+      servers: [
+        {
+          url: "http://localhost:3000",
+        },
+      ],
+    },
+    apis: ["./routes.js"],
+  };
+  
+  const specs = swaggerJSDoc(options);
+  app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs)
+  );
+
+  // Swagger
+
 
 mongoose.set('strictQuery', false);
 mongoose.connect(DB_URI)
--- routes.js
+++ routes.js
@@ -4,6 +4,41 @@ import * as controllers from "./controllers.js"
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * 
+ * tags:
+ *  name: Clientes
+ *  description: Clientes de la armeria
+ * /api/armeria/clientes:
+ *  get:
+ *      sumary: Devuelve todos los clientes
+ *      tags: [Cliente]
+ *      responses:
+ *          200:
+ *              description: Lista de los clientes
+ *              content:
+ *                  application/json:
+ *                      type: array
+ * /api/armeria/clientes/{id}:
+ *  get:
+ *      sumary: Devuelve todos los clientes
+ *      tags: [Cliente]
+ *      parameters:
+ *          - in: path
+ *            name: id
+ *            schema:
+ *              type: string
+ *            required: true
+ *            description: Id del cliente
+ *      responses:
+ *          200:
+ *              description: Lista de los clientes
+ *              content:
+ *                  application/json:
+ *                      type: array
+ *      
+ */
 router.get("/armeria/clientes", cors(), controllers.readClientes);
 
 router.post("/armeria/clientes", cors(), controllers.insertCliente);
